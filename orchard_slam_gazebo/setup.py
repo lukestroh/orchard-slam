d0802651bf7d220829b17bc6f357b6f064de8247
lukestroh <luke.strohbehn@gmail.com>
2026-01-31 08:25:43 +0000

    adding files for merge

--- orchard_slam_gazebo/setup.py
+++ orchard_slam_gazebo/setup.py
@@ -14,7 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob.glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'models'), glob.glob('models/*')),
+        # (os.path.join('share', package_name, 'models'), glob.glob('models/*')),
         (os.path.join('share', package_name, 'worlds'), glob.glob('worlds/*.sdf')),
     ],
     package_data={'': ['py.typed']},
