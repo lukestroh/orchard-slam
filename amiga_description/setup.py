7a64edeb2bf9b302a832687177b5ea618ffbc880
lukestroh <luke.strohbehn@gmail.com>
2026-01-31 04:30:51 +0000

    add initial packages

---
+++ amiga_description/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'amiga_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lukestroh',
+    maintainer_email='luke.strohbehn@gmail.com',
+    description='a description package for the Farm-ng Amiga robot',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
