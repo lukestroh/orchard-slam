71059c996103f8878c72d8e32b07ede5b4959d28
lukestroh <luke.strohbehn@gmail.com>
2026-01-31 07:05:02 +0000

    add basic bringup functions

--- orchard_slam_bringup/setup.py
+++ orchard_slam_bringup/setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+import os
+import glob
+
 package_name = 'orchard_slam_bringup'
 
 setup(
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob.glob('rviz/*.rviz')),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
