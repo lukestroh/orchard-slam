31751cb3841c05482fb2d32f0494b6ddd7125c99
lukestroh <luke.strohbehn@gmail.com>
2026-02-06 01:23:09 +0000

    updates to amiga_description launch, changed controllers file name

--- amiga_description/launch/amiga_gzsim_launch.py
+++ amiga_description/launch/amiga_gzsim_launch.py
@@ -10,67 +10,61 @@ from launch.actions import TimerAction
 
 import os
 
+
 def generate_launch_description():
     # Your xacro
-    amiga_share = get_package_share_directory('amiga_description')
-    xacro_file = os.path.join(amiga_share, 'urdf', 'amiga.urdf.xacro')
+    amiga_share = get_package_share_directory("amiga_description")
+    xacro_file = os.path.join(amiga_share, "urdf", "amiga.urdf.xacro")
 
     # Gazebo Sim launch
-    ros_gz_share = get_package_share_directory('ros_gz_sim')
+    ros_gz_share = get_package_share_directory("ros_gz_sim")
     gz_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(ros_gz_share, 'launch', 'gz_sim.launch.py')
-        ),
+        PythonLaunchDescriptionSource(os.path.join(ros_gz_share, "launch", "gz_sim.launch.py")),
         launch_arguments={
             # -r: run, -v 4: verbose, empty.sdf: empty world
-            'gz_args': '-r -v 4 empty.sdf'
-        }.items()
+            "gz_args": "-r -v 4 empty.sdf"
+        }.items(),
     )
 
     # Publish robot_description (still useful for TF and for spawning via topic)
-    robot_description = ParameterValue(
-        Command(['xacro', ' ', xacro_file]),
-        value_type=str
-    )
+    robot_description = ParameterValue(Command(["xacro", " ", xacro_file]), value_type=str)
     rsp = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'use_sim_time': True,
-            'robot_description': robot_description
-            }]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[{"use_sim_time": True, "robot_description": robot_description}],
     )
 
     clock_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
+        output="screen",
     )
 
     # Spawn the robot from robot_description topic
     spawn = TimerAction(
         period=3.0,
-        actions=[Node(
-            package='ros_gz_sim',
-            executable='create',
-            output='screen',
-            arguments=['-name', 'amiga', '-topic', 'robot_description', '-z', '0.3']
-        ),
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
-            output="screen",
-        ),
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
-            output="screen",
-        ),]
+        actions=[
+            Node(
+                package="ros_gz_sim",
+                executable="create",
+                output="screen",
+                arguments=["-name", "amiga", "-topic", "robot_description", "-z", "0.3"],
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
+                output="screen",
+            ),
+        ],
     )
-
 
     return LaunchDescription([gz_launch, rsp, spawn, clock_bridge])
